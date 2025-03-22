58a96f78003803223fd57c0805cc82df3f018546
Bautista <bautistaariza2001@gmail.com>
2025-03-21 23:07:23 -0300

    mesagge

---
+++ Ingles/mesagge-both.js
@@ -0,0 +1,20 @@
+const btn = document.getElementById('button-both');
+
+document.getElementById('form-both')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_ltrmtbt';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
